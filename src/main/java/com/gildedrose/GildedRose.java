07dc6846bbc7091e0fde26c8a4bbdd8d46b69f02
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-04-19 16:38:41 -0300

    Refactor: simplificação do método principal updateQuality()
    Simplifica o método updateQuality(), da classe GildedRose, através da criação de um for() que instancia o novo método updateItemQuality() passando os itens a serem atualizados. Refatoração conforme o padrão 'Decompose Conditional'.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,53 +12,53 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE_CHEESE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS_ITEM)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+        for (Item item : items) {
+            updateItemQuality(item);
+        }
+    }
+
+    public void updateItemQuality(Item item) {
+        if (!item.name.equals(AGED_BRIE_CHEESE) && !item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.quality > 0) {
+                if (!item.name.equals(SULFURAS_ITEM)) {
+                    item.quality = item.quality - 1;
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                if (item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
             }
-
-            if (!items[i].name.equals(SULFURAS_ITEM)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE_CHEESE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS_ITEM)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+        }
+        if (!item.name.equals(SULFURAS_ITEM)) {
+            item.sellIn = item.sellIn - 1;
+        }
+        if (item.sellIn < 0) {
+            if (!item.name.equals(AGED_BRIE_CHEESE)) {
+                if (!item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(SULFURAS_ITEM)) {
+                            item.quality = item.quality - 1;
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    item.quality = item.quality - item.quality;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }

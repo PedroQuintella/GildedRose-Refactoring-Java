59ab3e27ebd9b5c586f43a05e1c46f4802f3ecf9
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-04-20 23:40:10 -0300

    Refactor: substituição dos números mágicos por MIN_QUALITY e MAX_QUALITY
    Substitui os números mágicos nas condicionais de qualidade pelas constantes MIN_QUALITY e MAX_QUALITY criadas na classe GildedRose, conforme o padrão 'Replace Magic Literal'.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,8 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
+    public static final int MIN_QUALITY = 0;
+    public static final int MAX_QUALITY = 50;
     public static final String AGED_BRIE_CHEESE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS_ITEM = "Sulfuras, Hand of Ragnaros";
@@ -19,23 +21,23 @@ class GildedRose {
 
     public void updateItemQuality(Item item) {
         if (!item.name.equals(AGED_BRIE_CHEESE) && !item.name.equals(BACKSTAGE_PASSES)) {
-            if (item.quality > 0) {
+            if (item.quality > MIN_QUALITY) {
                 if (!item.name.equals(SULFURAS_ITEM)) {
                     decreaseQuality(item);
                 }
             }
         } else {
-            if (item.quality < 50) {
+            if (item.quality < MAX_QUALITY) {
                 increaseQuality(item);
 
                 if (item.name.equals(BACKSTAGE_PASSES)) {
                     if (item.sellIn < 11) {
-                        if (item.quality < 50) {
+                        if (item.quality < MAX_QUALITY) {
                             increaseQuality(item);
                         }
                     }
                     if (item.sellIn < 6) {
-                        if (item.quality < 50) {
+                        if (item.quality < MAX_QUALITY) {
                             increaseQuality(item);
                         }
                     }
@@ -45,10 +47,10 @@ class GildedRose {
         if (!item.name.equals(SULFURAS_ITEM)) {
             item.sellIn = item.sellIn - 1;
         }
-        if (item.sellIn < 0) {
+        if (item.sellIn < MIN_QUALITY) {
             if (!item.name.equals(AGED_BRIE_CHEESE)) {
                 if (!item.name.equals(BACKSTAGE_PASSES)) {
-                    if (item.quality > 0) {
+                    if (item.quality > MIN_QUALITY) {
                         if (!item.name.equals(SULFURAS_ITEM)) {
                             decreaseQuality(item);
                         }
@@ -57,7 +59,7 @@ class GildedRose {
                     item.quality = item.quality - item.quality;
                 }
             } else {
-                if (item.quality < 50) {
+                if (item.quality < MAX_QUALITY) {
                     increaseQuality(item);
                 }
             }
